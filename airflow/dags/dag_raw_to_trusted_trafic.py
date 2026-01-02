31de9475a821b1d35979cb01e2e6330f4d13c303
Ednilson Lomazi <65460604+ednilsonlomazi@users.noreply.github.com>
2026-01-02 11:15:37 -0300

    Melhora Performance: raw to trusted via microbatchs

--- airflow/dags/dag_raw_to_trusted_trafic.py
+++ airflow/dags/dag_raw_to_trusted_trafic.py
@@ -18,15 +18,23 @@ with DAG(
     'pipeline_trusted_tab_trafego',
     default_args=default_args,
     description='Gera tab_trafego na camada Trusted do Data Lakehouse',
-    schedule='0 6-18 * * *', # roda toda hora, entre as 6h e 18h
-    catchup=False, # começa a partir de agora (ignora os schedules passados)
+    schedule='0 6-18 * * *',
+    catchup=False,
     tags=['TCC', 'Spark'],
 ) as dag:
 
+    # a task setup garante que a pasta tab_trafego exista antes de iniciar o processo raw_to_trusted
+    task_setup = BashOperator(
+        task_id='garantir_tabela_trusted',
+        bash_command='python3 /opt/airflow/scripts/raw_to_trusted.py setup'
+    )
+
+    # chama os processos de raw para trusted
     for t in topicos:
-        executar_pyspark = BashOperator(
+        task_merge = BashOperator(
             task_id=f'processa_topico_{t}',
             bash_command=f'python3 /opt/airflow/scripts/raw_to_trusted.py {t}'
         )
 
-    executar_pyspark
+        # cada task do processo raw to trusted depende a task de setup
+        task_setup >> task_merge
--- scripts/raw_to_trusted.py
+++ scripts/raw_to_trusted.py
@@ -1,7 +1,14 @@
 from pyspark.sql import SparkSession
 from delta.tables import DeltaTable
+import sys
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
+from pyspark.sql.functions import from_json, col
 
-# Inicializa a sessão (mantendo suas configs de S3/MinIO)
+path_trusted = "s3a://trusted/tab_trafego"
+arg = sys.argv[1]
+path_raw = f"s3a://raw/{arg}"
+
+
 spark = SparkSession.builder \
     .appName("Merge-Raw-to-Trusted") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
@@ -13,36 +20,69 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
+schema_trusted = StructType([
+    StructField("data", StringType(), True),                
+    StructField("hora", IntegerType(), True),      
+    StructField("num_lote", StringType(), True),      
+    StructField("desc_praca", StringType(), True),
+    StructField("desc_sentido", StringType(), True),    
+    StructField("desc_tipo_pista", StringType(), True),
+    StructField("desc_tipo_passagem", StringType(), True),
+    StructField("desc_tipo_pagamento", StringType(), True),
+    StructField("desc_tipo_veiculo", StringType(), True),
+    StructField("qtd_veiculos", IntegerType(), True),
+    StructField("id_msn", StringType(), False), 
+    StructField("dt_generation", StringType(), True)
+])
 
-path_trusted = "s3a://trusted/tab_trafego"
+# Função que o Spark vai chamar para cada "pedaço" (micro-batch) novo de dados, <Near-realtime>
+def upsert_to_delta(microBatchDf, batchId):
+    print(f"Processando batch ID: {batchId}")
+    dt_trusted = DeltaTable.forPath(spark, path_trusted)
+    
+    dt_trusted.alias("trusted").merge(
+        microBatchDf.alias("updates"),
+        "trusted.id_msn = updates.id_msn"
+    ).whenMatchedUpdateAll() \
+    .whenNotMatchedInsertAll() \
+    .execute()
 
-topicos = ('l01', 'l03', 'l06')
-for topico in topicos:
 
-    path_raw = f"s3a://raw/{topico}"
-    print(f"--------- Iniciando processamento no topico: {topico} --------------")
-    try:
-        # 1. Lê os novos dados da RAW
-        df_novos_dados = spark.read.format("delta").load(path_raw)
-        
-        # 2. Verifica se a tabela Trusted já existe para fazer o Merge
-        if not DeltaTable.isDeltaTable(spark, path_trusted):
-            print(f"Caminho {path_trusted} não encontrado... criando-o pela primeira vez.")
-            df_novos_dados.write.format("delta").mode("overwrite").save(path_trusted)
-        else:
-            dt_trusted = DeltaTable.forPath(spark, path_trusted)
+if arg == 'setup':
+    if not DeltaTable.isDeltaTable(spark, path_trusted):
+        # Cria um DataFrame vazio com o schema definido
+        df_vazio = spark.createDataFrame([], schema_trusted)
+        df_vazio.write.format("delta").save(path_trusted)
+        print("Tabela Delta inicializada com sucesso.")
+    else:
+        print("A pasta já existe no MinIO. Setup ignorado.")
+    
+    spark.stop()
+    sys.exit(0)
 
-            print("Merge iniciado")
-            dt_trusted.alias("trusted").merge(
-                df_novos_dados.alias("updates"),
-                "trusted.id_msn = updates.id_msn"
-            ).whenMatchedUpdateAll() \
-            .whenNotMatchedInsertAll() \
-            .execute()
-            
-        print("Merge concluído com sucesso!")
 
-    except Exception as e:
-        print(f"Erro no Merge: {e}")
- 
-spark.stop()
+try:
+    
+    df_raw_stream = spark.readStream.format("delta").load(path_raw)
+
+    # faz o parse do json
+    df_novos_dados = df_raw_stream.select(
+        from_json(col("value").cast("string"), schema_trusted).alias("data")
+    ).select("data.*")
+
+    print(f"Iniciando escrita incremental para: {arg}")
+
+    # Trigger: AvailableNow: Isso faz o Spark processar apenas o que há de novo e PARAR (como uma DAG comum)
+    query = df_novos_dados.writeStream \
+        .foreachBatch(upsert_to_delta) \
+        .option("checkpointLocation", f"s3a://trusted/checkpoints/{arg}") \
+        .trigger(availableNow=True) \
+        .start()
+    
+    query.awaitTermination()
+    print("Processamento incremental concluído!")
+
+except Exception as e:
+    print(f"Erro no processamento: {e}")
+finally:
+    spark.stop()
